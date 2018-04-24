c6b5b09c09987a4c7c0e389910f628a59bd77ff2
vainawebuser <vainawebuser@gmail.com>
2018-04-24 15:13:50 -0300

    hora pronta

--- src/components/Relogio.vue
+++ src/components/Relogio.vue
@@ -1,60 +1,62 @@
 <template>
 	<div id="Clockapplication">
-		<form name="form_relogio" class="responsive">
-			<img class="h-ponteiro" src="../assets/relogio.png">
-			<input class="h-digital" type="text" name="relogio" size="10" v-model="hours">
-		</form>
+    	<h1 class="h-digital">{{aTime}}</h1>;
 	</div>
 </template>
 <script>
+  var getAtime = function() {
+  var date = new Date();
+  var hour = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+  var minute = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+  var second = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+  var str_hour = hour + ':' + minute + ':' + second;
+  return str_hour;
+};
+
 	export default{
    	name:'hora',
-   	data(){
-   		return{
-   			//momentoAtual:'',
-   			hours:''
-   		}
-   	},
-   	created(){
-   			var momentoAtual = new Date()
-   			return this.hours = momentoAtual.getHours() + ':' + momentoAtual.getMinutes()  + ':' + momentoAtual.getSeconds()
-   	}
-   	// computed:{
-   	// 	hours(){
-   	// 		var momentoAtual = new Date()
-   	// 		return momentoAtual.getHours() + ':' + momentoAtual.getMinutes()  + ':' + momentoAtual.getSeconds()
-   	// 	}
-   	// }
+   	data: function() {
+    return {
+      time: '00:00:00'
+    };
+  },		
+	 computed: {
+		aTime: function() {
+		  var self = this;
+		  setInterval(function() {
+		    self.time = getAtime();
+		  }, 1000);
+		  return self.time;
+		}
+	}
 }
 </script>
 <style scoped>
 	#Clockapplication{
-		background-image: url(../assets/fundo.jpg);
+		background-image: url(../assets/fundo2.jpg);
+		/*background-color: black;	*/
 		min-height: 100vh;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
-	.responsive{
-		display: flex;
-		flex-direction: column;
-	}
-	.h-ponteiro{	
-		width: 20vh;
-		margin: 0 0 0 80px;		
+	#Clockapplication:hover{
+		background-image: url(../assets/fundo.jpg);
 	}
 	.h-digital{
 		background: white;
 		color: black;
-		width: 40vh;
-		height: 14vh;
-		font-size: 50px;
-		margin: 10px;
+		width: 100vh;
+		height: 30vh;
+		font-size: 150px;
 		text-align: center;
+		border:solid;
+		border-color: blue;
 	}
-	.data{
-		background: white;
-		font-size: 30px;
-		margin: 10px;
+	.h-digital:hover{
+		border:solid;
+		border-color: red;
+		background-color: black;
+		color: white;
 	}
 </style>
